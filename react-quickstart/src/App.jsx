0596ce07649f78d7d635a5da99e018e7ad33fb39
Daniel Latina <latinadaniel4@gmail.com>
2024-10-07 16:17:55 +0800

    Finished Adding Styles and Displaying Data

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -3,6 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const user = {
+  name: 'Travis Scott',
+  imageUrl: 'https://us-tuna-sounds-images.voicemod.net/134ae266-5114-42f5-920a-034908a52858-1688717930888.png',
+  imageSize: 90,
+}
+
+function Profile(){
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}
 function AboutPage(){
   return(
     <>
@@ -16,16 +38,27 @@ function AboutPage(){
 
 function MyButton() {
   return (
+    <>
+    <a href="https://youtu.be/znqA2Qdw7HA?si=rG2fkmPvQ32Qx_Ds" target='blank'>
     <button>
-      I'm a button
+      I am Sorry
     </button>
+    </a>
+    </>
+    
   );
 }
+
+
 
 export default function MyApp() {
   return (
     <div>
+      <h1>Welcome to my app</h1>
       <AboutPage/>
+      <Profile />
+      <br />
+      
       <MyButton />
     </div>
   );
