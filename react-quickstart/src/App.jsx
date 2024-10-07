1417fef8e43ba7bbf485991ec07a1d352f90b07d
Daniel Latina <latinadaniel4@gmail.com>
2024-10-07 17:39:49 +0800

    finished Rendering Lists

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -44,6 +44,30 @@ function Profile() {
     );
   }
 
+function ShoppingList(){
+  const products = [
+    {title:"Cabbage", isFruit: false, id:1},
+    {title:"Garlic", isFruit: false, id:2},
+    {title:"Apple", isFruit: true, id:3},
+  ]
+
+  const listItems = products.map(product =>
+    <li
+    key = {product.id}
+    style={{
+      color: product.isFruit ? 'magenta' : 'darkgreen'
+    }}>
+
+      {product.title}
+
+    </li>
+  );
+
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
 // function ConRender(){
 //   let content;
 //   if (isLoggedIn) {
@@ -65,6 +89,10 @@ export default function MyApp() {
       <MyButton />
       <AboutPage/>
       <Profile/>
+      <div className='ShoppingList'>
+        <ShoppingList/>
+      </div>
+      
     </div>
   );
 }
