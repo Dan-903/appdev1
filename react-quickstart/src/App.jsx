faa3cb7e24462b6ecc104a7c854430acf4edc4c2
Daniel Latina <latinadaniel4@gmail.com>
2024-10-08 21:02:29 +0800

    Added Writing markup with JSX section

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,18 +1,20 @@
 import './App.css'
 
-
-function MyButton(){
-  
-  return(
-    <button>I'm a button</button>
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Hello there.<br />How do you do?</p>
+    </>
   );
 }
+
 function App() {
   
 
   return (
     <>
-    <MyButton/>
+    <AboutPage/>
     </>
   )
 }
