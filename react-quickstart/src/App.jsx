c652b4fcca3603adbe8fce0276d293b4f400cf81
Daniel Latina <latinadaniel4@gmail.com>
2024-10-08 21:00:53 +0800

    Added creating and nesting components section

---
+++ react-quickstart/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+
+function MyButton(){
+  
+  return(
+    <button>I'm a button</button>
+  );
+}
+function App() {
+  
+
+  return (
+    <>
+    <MyButton/>
+    </>
+  )
+}
+
+export default App
