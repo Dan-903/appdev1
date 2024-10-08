c0d880f8c966745d7fe309425c51200d9f363252
Daniel Latina <latinadaniel4@gmail.com>
2024-10-08 21:15:52 +0800

    Added Responding to events section

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,35 +1,16 @@
 import './App.css'
 
-const products = [
-  { title: 'Cabbage', isFruit: false, id: 1 },
-  { title: 'Garlic', isFruit: false, id: 2 },
-  { title: 'Apple', isFruit: true, id: 3 },
-];
-
-
-function ShoppingList() {
-  const listItems = products.map(product =>
-    <li
-      key={product.id}
-      style={{
-        color: product.isFruit ? 'magenta' : 'darkgreen'
-      }}
-    >
-      {product.title}
-    </li>
-  );
+function MyButton() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
 
   return (
-    <>
-        <div className='shoppingList'>
-            <ul>{listItems}</ul>
-        </div>
-
-    </>
-
+    <button onClick={handleClick}>
+      Click me
+    </button>
   );
 }
-
 
 
 function App() {
@@ -37,7 +18,7 @@ function App() {
 
   return (
     <>
-    <ShoppingList/>
+    <MyButton/>
     </>
   )
 }
